0777f76ea3353a6ac685a906d6b14ff56906a7b6
Breanna Hartman <breanna.e.hartman@gmail.com>
2015-05-13 20:19:25 -0600

    Edits & w5d2 Exercise
    made some edits to better match the pages and input fields w/ the db
    schema. also completed exercise: working with sessions and params

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -16,11 +22,31 @@ get '/signup' do
 end
 
 post '/login' do
-  redirect '/'
+  username = params[:login_username]
+  password = params[:login_password]
+
+  user = User.find_by(username: username)
+  if user.password == password
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect'/login'
+  end
 end
 
 post '/signup' do
-  redirect '/'
+  username = params[:signup_username]
+  password = params[:signup_password]
+  email = params[:signup_email]
+
+  user = User.find_by(username: username)
+  if user
+    redirect '/login'
+  else
+    user = User.create(username: username, password: password, email: email)
+    session[:user_id] = user.id
+    redirect '/'
+  end
 end
 
 post 'profile' do
